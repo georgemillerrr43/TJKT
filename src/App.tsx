b8d4e477b47184288b3e7c592639c95420abc4bb
joji <georgemillerrr43@gmail>
2026-03-20 21:11:25 +0700

    update package-App_pt1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Ganti ke HashRouter untuk GitHub Pages
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import Programs from './pages/Programs';
